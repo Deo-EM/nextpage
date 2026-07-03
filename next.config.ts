4bd967fb5e14de63b33a033270e92aa802a0e72a
dupeng <dupeng@vzan.com>
2026-07-03 14:41:49 +0800

    feat: next init

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+if (process.env.NODE_ENV === "development") {
+  initOpenNextCloudflareForDev();
+}
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
